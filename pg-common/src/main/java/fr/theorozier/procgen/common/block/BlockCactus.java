f2639f31e745416ef37b6efcc674763e4efb5977
mindstorm38 <theorozier38@gmail.com>
2020-01-08 19:17:33 +0100

    Added right collision to cactus block.

--- pg-common/src/main/java/fr/theorozier/procgen/common/block/BlockCactus.java
+++ pg-common/src/main/java/fr/theorozier/procgen/common/block/BlockCactus.java
@@ -1,5 +1,10 @@
 package fr.theorozier.procgen.common.block;
 
+import fr.theorozier.procgen.common.block.state.BlockState;
+import fr.theorozier.procgen.common.phys.AxisAlignedBB;
+
+import java.util.List;
+
 public class BlockCactus extends Block {
 	
 	public BlockCactus(int uid, String identifier) {
@@ -14,6 +19,11 @@ public class BlockCactus extends Block {
 	@Override
 	public BlockRenderLayer getRenderLayer() {
 		return BlockRenderLayer.CUTOUT;
+	}
+	
+	@Override
+	public void getStateCollision(BlockState state, List<AxisAlignedBB> boundingBoxes) {
+		boundingBoxes.add(new AxisAlignedBB(0.0625f, 0, 0.0625f, 0.9375, 1, 0.9375));
 	}
 	
 }
