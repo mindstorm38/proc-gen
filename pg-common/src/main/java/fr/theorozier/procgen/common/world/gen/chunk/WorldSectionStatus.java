41a609787967bb8fef4e67cc2ef70aed81e06392
mindstorm38 <theorozier38@gmail.com>
2020-01-24 10:56:55 +0100

    Added identifier registration of WorldSectionStatus

--- pg-common/src/main/java/fr/theorozier/procgen/common/world/gen/chunk/WorldSectionStatus.java
+++ pg-common/src/main/java/fr/theorozier/procgen/common/world/gen/chunk/WorldSectionStatus.java
@@ -2,50 +2,76 @@ package fr.theorozier.procgen.common.world.gen.chunk;
 
 import fr.theorozier.procgen.common.world.chunk.WorldServerSection;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public abstract class WorldSectionStatus {
 	
-	public static final WorldSectionStatus EMPTY = new WorldSectionStatus("empty", null, BIOMES) {
+	private static final Map<String, WorldSectionStatus> statusRegister = new HashMap<>();
+	
+	private static void registerStatus(WorldSectionStatus status) {
+		statusRegister.put(status.getIdentifier(), status);
+	}
+	
+	public static final WorldSectionStatus EMPTY = new WorldSectionStatus("empty", null, "biomes") {
 		public void generate(ChunkGenerator generator, WorldServerSection section) {}
 	};
 	
-	public static final WorldSectionStatus BIOMES = new WorldSectionStatus("biomes", EMPTY, BASE) {
+	public static final WorldSectionStatus BIOMES = new WorldSectionStatus("biomes", "empty", "base") {
 		public void generate(ChunkGenerator generator, WorldServerSection section) {
 			generator.genBiomes(section, section.getSectionPos());
 		}
 	};
 	
-	public static final WorldSectionStatus BASE = new WorldSectionStatus("base", BIOMES, SURFACE) {
+	public static final WorldSectionStatus BASE = new WorldSectionStatus("base", "biomes", "surface") {
 		public void generate(ChunkGenerator generator, WorldServerSection section) {
 			generator.genSectionBase(section, section.getSectionPos());
 		}
 	};
 	
-	public static final WorldSectionStatus SURFACE = new WorldSectionStatus("surface", BASE, FEATURES) {
+	public static final WorldSectionStatus SURFACE = new WorldSectionStatus("surface", "base", "features") {
 		public void generate(ChunkGenerator generator, WorldServerSection section) {
 			generator.genSurface(section, section.getSectionPos());
 		}
 	};
 	
-	public static final WorldSectionStatus FEATURES = new WorldSectionStatus("features", SURFACE, FINISHED) {
+	public static final WorldSectionStatus FEATURES = new WorldSectionStatus("features", "surface", "finished") {
 		public void generate(ChunkGenerator generator, WorldServerSection section) {
 			generator.genFeatures(section, section.getSectionPos());
 		}
 	};
 	
-	public static final WorldSectionStatus FINISHED = new WorldSectionStatus("finished", FEATURES, null) {
+	public static final WorldSectionStatus FINISHED = new WorldSectionStatus("finished", "features", null) {
 		public void generate(ChunkGenerator generator, WorldServerSection section) {}
 	};
+	
+	static {
+		
+		registerStatus(EMPTY);
+		registerStatus(BIOMES);
+		registerStatus(BASE);
+		registerStatus(SURFACE);
+		registerStatus(FEATURES);
+		registerStatus(FINISHED);
+		
+		statusRegister.values().forEach(status -> {
+			status.prev = status.prevId == null ? null : statusRegister.get(status.prevId);
+			status.next = status.nextId == null ? null : statusRegister.get(status.nextId);
+		});
+		
+	}
 	
 	// Class //
 	
 	private final String identifier;
-	private final WorldSectionStatus prev, next;
+	private final String prevId, nextId;
+	private WorldSectionStatus prev, next;
 	
-	public WorldSectionStatus(String identifier, WorldSectionStatus prev, WorldSectionStatus next) {
+	public WorldSectionStatus(String identifier, String prev, String next) {
 		
 		this.identifier = identifier;
-		this.prev = prev;
-		this.next = next;
+		this.prevId = prev;
+		this.nextId = next;
 		
 	}
 	
