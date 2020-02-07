02d238b225f8c3aa3c2e1b1eaf8bfe032235dd68
mindstorm38 <theorozier38@gmail.com>
2020-02-07 21:46:27 +0100

    Reformat

--- pg-client/src/main/java/fr/theorozier/procgen/client/ProcGenGame.java
+++ pg-client/src/main/java/fr/theorozier/procgen/client/ProcGenGame.java
@@ -292,8 +292,6 @@ public class ProcGenGame extends DefaultRenderGame<ProcGenGame> implements Windo
 					entity.setPositionInstant(cam.getX(), cam.getY() - 2f, cam.getZ());
 					entity.setVelocity(Math.cos(Math.PI * -0.5 + cam.getYaw()), Math.sin(cam.getPitch()), Math.sin(Math.PI * -0.5 + cam.getYaw()));
 
-					// entity.setState(Blocks.CACTUS.getDefaultState());
-					
 					serverWorld.spawnEntity(entity);
 					
 				}
