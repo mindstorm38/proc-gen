3d2824913cc2ffc043601a057d47fc172d78dd26
mindstorm38 <theorozier38@gmail.com>
2020-02-18 22:54:33 +0100

    Added TODO in SectionPositioned for future urgent rework.

--- pg-common/src/main/java/fr/theorozier/procgen/common/world/position/SectionPositioned.java
+++ pg-common/src/main/java/fr/theorozier/procgen/common/world/position/SectionPositioned.java
@@ -2,6 +2,14 @@ package fr.theorozier.procgen.common.world.position;
 
 public interface SectionPositioned {
 
+	// TODO Need to rework this package, in order to extends abstract class instead
+	//      of implementing indenpendently these interfaces.
+	//      Currently it's an issue because we (and it will no longer have to be done)
+	//      use interface has keys for maps, and for example if we have a map mapped by
+	//      SectionPositioned keys, we are still able to put and call contains with
+	//      BlockPositioned and it's an issue because classes extending BlockPositioned
+	//      don't have this same hashcode() implementation.
+	
 	int getX();
 	int getZ();
 	
